e259bf104d4dbceffa81dc77b1d6e3c9fd2a2bcd
Marek Wallner <walle.sangala@gmail.com>
2019-11-14 19:09:06 +0100

    Uprava Baseline

--- src/Model.py
+++ src/Model.py
@@ -6,18 +6,15 @@ class Baseline(keras.models.Sequential):
     def __init__(self):
         super(Baseline, self).__init__()
         self.add(InputLayer(input_shape=[64, 64, 1]))
-        self.add(Conv2D(filters=32, kernel_size=5, strides=1, padding="same", activation="relu"))
-        self.add(MaxPool2D(pool_size=5, padding="same"))
+        self.add(Conv2D(filters=16, kernel_size=11, strides=4, padding="same", activation="relu"))
+        self.add(MaxPool2D(pool_size=3, strides=2, padding="same"))
 
-        self.add(Conv2D(filters=50, kernel_size=5, strides=1, padding="same", activation="relu"))
-        self.add(MaxPool2D(pool_size=5, padding="same"))
+        self.add(Conv2D(filters=20, kernel_size=5, strides=1, padding="same", activation="relu"))
+        self.add(MaxPool2D(pool_size=3, strides=2, padding="same"))
 
-        self.add(Conv2D(filters=80, kernel_size=5, strides=1, padding="same", activation="relu"))
-        self.add(MaxPool2D(pool_size=5, padding="same"))
+        self.add(Conv2D(filters=30, kernel_size=3, strides=1, padding="same", activation="relu"))
+        self.add(MaxPool2D(pool_size=3, strides=2, padding="same"))
 
-        self.add(Dropout(0.25))
         self.add(Flatten())
-        self.add(Dense(512, activation="relu"))
-        self.add(Dropout(rate=0.5))
+        self.add(Dense(48, activation="relu"))
         self.add(Dense(2, activation="softmax"))
-
