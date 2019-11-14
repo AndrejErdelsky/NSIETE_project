76437124f7fb20b1798c82e8dfca8c32fbab2b30
Marek Wallner <walle.sangala@gmail.com>
2019-11-14 18:30:35 +0100

    Uprava Baseline

--- src/Model.py
+++ src/Model.py
@@ -3,27 +3,21 @@ from keras.layers import *
 
 
 class Baseline(keras.models.Sequential):
-
     def __init__(self):
         super(Baseline, self).__init__()
-        self.model_layers = [
-            InputLayer(input_shape=[64, 64, 1]),
-            Conv2D(filters=32, kernel_size=5, strides=1, padding="same", activation="relu"),
-            MaxPool2D(pool_size=5, padding="same"),
+        self.add(InputLayer(input_shape=[64, 64, 1]))
+        self.add(Conv2D(filters=32, kernel_size=5, strides=1, padding="same", activation="relu"))
+        self.add(MaxPool2D(pool_size=5, padding="same"))
 
-            Conv2D(filters=32, kernel_size=5, strides=1, padding="same", activation="relu"),
-            MaxPool2D(pool_size=5, padding="same"),
+        self.add(Conv2D(filters=50, kernel_size=5, strides=1, padding="same", activation="relu"))
+        self.add(MaxPool2D(pool_size=5, padding="same"))
 
-            Conv2D(filters=32, kernel_size=5, strides=1, padding="same", activation="relu"),
-            MaxPool2D(pool_size=5, padding="same"),
-            Dropout(0.25),
-            Flatten(),
-            Dense(512, activation="relu"),
-            Dropout(rate=0.5),
-            Dense(2, activation="softmax")
-        ]
+        self.add(Conv2D(filters=80, kernel_size=5, strides=1, padding="same", activation="relu"))
+        self.add(MaxPool2D(pool_size=5, padding="same"))
 
-    def call(self, x):
-        for layer in self.model_layers:
-            x = layer(x)
-        return x
+        self.add(Dropout(0.25))
+        self.add(Flatten())
+        self.add(Dense(512, activation="relu"))
+        self.add(Dropout(rate=0.5))
+        self.add(Dense(2, activation="softmax"))
+
