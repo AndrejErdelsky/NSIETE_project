927e5f49612c700a7ced44ff87a9bee9a4f00c2e
Marek Wallner <walle.sangala@gmail.com>
2019-11-24 14:21:30 +0100

    uprava modelu, evaluacia v zvlast subore

--- src/Model.py
+++ src/Model.py
@@ -1,12 +1,12 @@
-import keras.models
+from keras.models import Sequential
 from keras.layers import *
 
 
-class Baseline(keras.models.Sequential):
+class Baseline(Sequential):
     def __init__(self):
-        super(Baseline, self).__init__()
-        self.add(InputLayer(input_shape=[64, 64, 1]))
-        self.add(Conv2D(filters=16, kernel_size=11, strides=4, padding="same", activation="relu"))
+        super(Baseline, self).__init__(name='Baseline')
+        # self.add(InputLayer(input_shape=[64, 64, 1]))
+        self.add(Conv2D(filters=16, kernel_size=11, strides=4, padding="same", activation="relu",input_shape=(64, 64, 1)))
         self.add(MaxPool2D(pool_size=3, strides=2, padding="same"))
 
         self.add(Conv2D(filters=20, kernel_size=5, strides=1, padding="same", activation="relu"))
